7977e9ae216cc27c1dd599d0e4669b01f83a281e
jcaf31 <jafernande406@gmail.com>
2023-08-29 14:55:35 -0600

    Ticket CY-8 is implemented successfully

--- cypress/e2e/cypress_soucedemo_project/inventory.cy.js
+++ cypress/e2e/cypress_soucedemo_project/inventory.cy.js
@@ -1,13 +1,14 @@
 describe('Inventory', () => {
+
   let fixture;
-  let countItems;
+
   beforeEach(() => {
     cy.fixture('soucedemo.json').then((fixture_from_fixtures) => {
       fixture = fixture_from_fixtures;
     });
   })
   it('Validate all products contain expected information', () => {
-    
+
     let priceRegEx = new RegExp('^\\$[\\d]+\\.[\\d]{2}');
     let descRegEx = new RegExp('^.{5,}$');  
     let itemNameRegEx = new RegExp('^[A-Z][a-z]+(\\s[A-Z](-[A-Z])?[a-z]+)+$');
@@ -18,11 +19,11 @@ describe('Inventory', () => {
     cy.get('[data-test="login-button"]').click();
     cy.url().should('eq', 'https://www.saucedemo.com/inventory.html');
     cy.get('.inventory_item').then(($numberOfItems) => {
-      countItems = $numberOfItems.length;
+      let countItems = $numberOfItems.length;
       for(let i=0; i<countItems; i++){
         cy.get('.inventory_item_img a img').eq(i).invoke('attr', 'src').should('match', new RegExp('.jpg$'));
-        cy.get('.inventory_item_price').eq(i).then(($element) => {
-          expect($element.text()).to.match(priceRegEx);
+        cy.get('.inventory_item_price').eq(i).then(($itemPrice) => {
+          expect($itemPrice.text()).to.match(priceRegEx);
         })
         cy.get('.inventory_item_desc').eq(i).invoke('text').should('match', descRegEx);
         cy.get('.inventory_item_name').eq(i).invoke('text').should('match', itemNameRegEx);
--- cypress/e2e/cypress_soucedemo_project/shopping_cart.cy.js
+++ cypress/e2e/cypress_soucedemo_project/shopping_cart.cy.js
@@ -14,8 +14,8 @@ describe('Shopping Cart', () => {
   })
   it('Shopping cart - validate the number of items selected on products page is equal to the cart page', () => {
 
-    let itemsSelected;
-
+    let itemsBadgeSelected;
+    let numberOfItemsToAdd;
     cy.get("#user-name").type(fixture.user_name)
     cy.get('[data-test="password"]').type(fixture.password)
     cy.get('[data-test="login-button"]').click()
@@ -24,16 +24,19 @@ describe('Shopping Cart', () => {
     cy.get('.title').contains('Products')
     cy.get('.inventory_item').then(($invItems) => {
       const countItems = $invItems.length;
-      const numberOfItemsToAdd = getRandomInt(countItems) + 1;
-
+      numberOfItemsToAdd = getRandomInt(countItems) + 1;
       for (let i = 0; i<numberOfItemsToAdd; i++){
         cy.get('.btn').eq(i).click();
       }
-      
-      cy.get('.shopping_cart_badge').then(($badgeValue) => {
-        itemsSelected = $badgeValue[0].innerText;
-        expect(Number(itemsSelected)).to.equal(numberOfItemsToAdd, 'The number of items selected was not equal to the number of items displayed in cart badge');
-      })
+    })
+    cy.get('.shopping_cart_badge').then(($badgeValue) => {
+      itemsBadgeSelected = $badgeValue[0].innerText;
+      expect(Number(itemsBadgeSelected)).to.equal(numberOfItemsToAdd, 'The number of items selected were not equal to the number of items displayed in cart badge');
+    })
+    cy.get('#shopping_cart_container').click();
+    cy.get('.cart_item').then(($invCartItems) => {
+      const countCartItems = $invCartItems.length;
+      expect(countCartItems).to.equal(Number(itemsBadgeSelected), 'The number of items displayed on cart page were not equal to the number of items displayed in cart badge');
     })
   })
 })
